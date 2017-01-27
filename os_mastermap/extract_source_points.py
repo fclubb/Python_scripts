2ce5c11c20f7fd485c219a8692532d21ee6967a2
fclubb <f.clubb@ed.ac.uk>
2017-01-27 09:55:32 +0000

    Final changes to source points script

--- os_mastermap/extract_source_points.py
+++ os_mastermap/extract_source_points.py
@@ -11,6 +11,7 @@ def extract_source_points(DataDirectory, catchment_shapefile, points_shapefile):
     from shapely.geometry import shape, Polygon, Point
     import csv
     from matplotlib import pyplot as plt
+    from operator import itemgetter
 
 
     # plot for testing
@@ -23,7 +24,6 @@ def extract_source_points(DataDirectory, catchment_shapefile, points_shapefile):
     with collection (DataDirectory+catchment_shapefile, 'r') as input:
         #schema = input.schema.copy()
         for f in input:
-            print f
             poly = Polygon(shape(f['geometry']))
             shapes.append(Polygon(shape(f['geometry'])))
             catchment_ids.append(f['properties']['OBJECTID'])
@@ -33,12 +33,14 @@ def extract_source_points(DataDirectory, catchment_shapefile, points_shapefile):
     # read in the hydro points
     with collection (DataDirectory+points_shapefile, 'r') as input:
         points=[]
+        point_ids=[]
         for p in input:
             # only append the sources
             if p['properties']['HYDRONODE_'] == 'source':
                 point_x = p['geometry']['coordinates'][0][0]
                 point_y = p['geometry']['coordinates'][0][1]
                 points.append(Point(p['geometry']['coordinates'][0][0], p['geometry']['coordinates'][0][1]))
+                point_ids.append(p['properties']['OBJECTID'])
                 ax.scatter(point_x, point_y, color='red')
 
     # save figure for debug
@@ -53,26 +55,6 @@ def extract_source_points(DataDirectory, catchment_shapefile, points_shapefile):
     print "Number of catchments: ", n_shapes
 
     # for each polygon, check for points inside it
-    # for i in range(len(shapes)):
-    #     this_csv_name = DataDirectory+'MM_sources_'+str(catchment_ids[i])+'.csv'
-    #     print this_csv_name
-    #     coords = [[] for i in range(2)]
-    #     n_points = len(points)
-    #     for j in range(n_points):
-    #         #print shapes[i].contains(points[j])
-    #         if points[j].within(shapes[i]) == True:
-    #             #push points back to csv
-    #             print points[j].x, points[j].y
-    #             coords[0].append(points[j].x)
-    #             coords[1].append(points[j].y)
-    #             # remove this point, as it's already been written to csv. This should improve efficiency
-    #             points.remove(points[j])
-    #             n_points = len(points)
-    #     # now write these points to a csv file
-    #     with open(this_csv_name, 'wb') as f:
-    #         writer = csv.writer(f)
-    #         writer.writerows(zip(*coords))
-
     catchment_points = []
     for i in range(n_points):
         for j in range(n_shapes):
@@ -83,17 +65,24 @@ def extract_source_points(DataDirectory, catchment_shapefile, points_shapefile):
     for catchment_id in catchment_ids:
         # write csv file for each catchment ID
         point_indices = [i for i, x in enumerate(catchment_points) if catchment_points[i] == catchment_id]
-        print point_indices
+        these_points = itemgetter(*point_indices)(points)
+        these_ids = itemgetter(*point_indices)(point_ids)
+        cols = [[] for i in range(3)]
+        for i in range(len(these_points)):
+            cols[0].append(these_ids[i])
+            cols[1].append(these_points[i].x)
+            cols[2].append(these_points[i].y)
 
         this_csv_name = DataDirectory+'MM_sources_'+str(catchment_id)+'.csv'
         with open(this_csv_name, 'wb') as f:
-            writer = csv.writer(f, delimeter=',')
-            writer.writerows()
+            writer = csv.writer(f)
+            writer.writerow(['OBJECTID', 'X', 'Y'])
+            writer.writerows(zip(*cols))
 
 if __name__ == '__main__':
 
-    DataDirectory = '/home/s0923330/Datastore/5m_dems/scotland/Catchment_boundaries/'
+    DataDirectory = '/home/s0923330/Datastore/5m_dems/scotland/Catchment_boundaries/sources_all_catchments/'
     #DataDirectory = 'Z:\\5m_dems\\scotland\\Catchment_boundaries\\'
-    catchment_shapefile = 'tay_catchment.shp'
-    points_shapefile = 'hydro_nodes_tay.shp'
+    catchment_shapefile = 'catchments_scotland.shp'
+    points_shapefile = 'hydro_nodes_scotland.shp'
     extract_source_points(DataDirectory,catchment_shapefile,points_shapefile)
